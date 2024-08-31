829cca1611187342dbf75b337c2b0034852b2532
Memoonabibi <memoonabibi794@gmail.com>
2024-08-31 09:01:04 +0500

    commit

--- app/src/androidTest/java/com/example/lazycolumn/LazyColumn.kt
+++ app/src/androidTest/java/com/example/lazycolumn/LazyColumn.kt
@@ -1,8 +1,9 @@
-package com.example.lazycolumn
-
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -11,25 +12,33 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@Preview(showSystemUi = true,
-    showBackground = true)
+@Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun LazyColumnFunction() {
+fun ContactList() {
     LazyColumn(modifier = Modifier.padding(all = 20.dp)) {
         item {
-            Text(
-                text = "Contact List",
-                fontWeight = FontWeight.ExtraBold,
-                fontSize = 30.sp
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 30.dp),
+                contentAlignment = Alignment.Center
             )
+            {
+                Text(
+                    text = "My Contacts",
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 35.sp
+                )
+            }
 
-            Spacer(modifier = Modifier.padding(top = 30.dp))
+            Spacer(modifier = Modifier.size(15.dp))
 
             Row {
                 Icon(
